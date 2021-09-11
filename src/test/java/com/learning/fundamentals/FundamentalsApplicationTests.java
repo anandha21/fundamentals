b54822353aff52770a91eccdff2834daf41b6ac9
Anandha Krishnan S <anandhakrish86@gmail.com>
2021-09-11 16:23:36 +0530

    Initial Commit

---
+++ src/test/java/com/learning/fundamentals/FundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.fundamentals;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsApplicationTests {
+
+	@Test
+	 void contextLoads() {
+	}
+
+}
